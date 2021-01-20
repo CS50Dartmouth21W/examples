28f289152eb39ae7c85125adb0c168bd713de1d5
ccpalmer <ccpalmer@dartmouth.edu>
2021-01-19 22:02:33 -0500

    examples for c2

--- files.c
+++ files.c
@@ -17,13 +17,18 @@ int main(void)
    int    n;
    FILE   *ifp;
 
-   ifp = fopen("input", "r");
+   ifp = fopen("input.txt", "r");
    assert(ifp != NULL);
-   // fgets reads in a string until it gets the newline (\n),  EOF character or reads MAXSTRING-1
-   // characters. If it reads a newline it appends to the end of the array. This is the case
-   // for the names file. The contents of the file have a string then a \n. 
+
+   // fgets reads in a string until it gets the newline (\n),
+   // EOF character or reads MAXSTRING-1 characters. 
+   // If it reads a newline it appends to the end of the array. 
+   // This is the case for the names file. 
+   // The contents of the file have a string then a \n. 
+   //
    val = fgets(input, MAXSTRING, ifp);
    assert(val != NULL);
+
    fclose(ifp);
 
    /*
@@ -33,20 +38,27 @@ int main(void)
 
    // Note below we print out the /n with the %s when looking at the output.
 
-   printf("string is \"%s\" and value at the end of string is the \\n character %d\n", input, *(input + n - 1));
+   printf("\nstring is \"%s\"", input); 
+   printf("\tand the value at the end of string is the \\n character: %d\n", 
+          input[n - 1]);
 
-   // So that the string will operate correctly we have to replace the \n with the NULL (\0) character
+   // So that the string will operate correctly we have to replace the \n 
+   // with the NULL (\0) character
 
-   *(input + n - 1) = '\0';
+   input[n-1] = '\0';  // same, using pointers: *(input + n - 1)
 
-   printf("string is \"%s\" and value at the end of string is the \\0 character %d\n", input, *(input + n - 1));
+   printf("string is \"%s\"\n",input);  
+   printf("\tand the value at the end of string is now the \\0 character: %d\n", 
+           input[n - 1]);
 
-   // sscanf parses the input string.  %*s means "match a string and then throw it away", and specific
-   // characters must be matched by the format string it the sscanf terminates.
+   // sscanf parses the input string.  
+   // %*s means "match a string and then throw it away", and specific
+   // characters must be matched by the format string or the sscanf terminates.
    cnt = sscanf(input, "%d , %*s %% %c %5s %s", &i, &c, string, &string[5]);
 
-   // Note that when C encounters two successive string literals with onyl whitespace between them, 
-   // C will combine them intl a single string for you.  Nice!  We use that here.
+   // Note that when C encounters two successive string literals with only 
+   // whitespace between them, C will combine them intl a single string for you.
+   // Nice!  We use that here.
 
    printf("%s%s%s%c%s%d%s%s%s%s%s%d%s\n",
       "---\n"
